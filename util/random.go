a253cf2b7e93fea4426b79320b082704e9bfe91a
Hoang Tran <hoangtk.0100@gmail.com>
2022-08-22 17:46:51 +0700

    Writed unit tests for database CRUD

---
+++ util/random.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// init is called automatically when the package is first used
+func init() {
+	rand.Seed(time.Now().UnixNano()) // seed the random number generator
+}
+
+// RandomInt generates a random integer between min and max (inclusive)
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	length := len(alphabet)
+
+	for index := 0; index < n; index++ {
+		c := alphabet[rand.Intn(length)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(8)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD", "JPY"}
+	length := len(currencies)
+	return currencies[rand.Intn(length)]
+}
