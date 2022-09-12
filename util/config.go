303db3c10d83932a1748e71552250181a3542b8d
Hoang Tran <hoangtk.0100@gmail.com>
2022-09-12 15:55:08 +0700

    Loaded config from file and environment variables

---
+++ util/config.go
@@ -0,0 +1,29 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config stores all the configurations of application
+// The values are read by viper from a configuration file or environment variables
+type Config struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configurations from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	// Automatically overrides values with the the value of corresponding environment variable if they exist
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
