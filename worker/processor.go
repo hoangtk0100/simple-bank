105dc985d6f255a6064162a09c16fc748ebcc44a
hoangtk0100 <hoangtk.0100@gmail.com>
2023-01-14 15:35:11 +0700

    feat(async-worker): Implemented background worker with Redis task queue to create user and send verify email (#27)
    * feat(async-worker): Implemented background worker with Redis task queue
    
    * feat(async-worker): Integrated async worker to server

---
+++ worker/processor.go
@@ -0,0 +1,50 @@
+package worker
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+	db "github.com/hoangtk0100/simple-bank/db/sqlc"
+)
+
+const (
+	QueueCritical = "critical"
+	QueueDefault  = "default"
+	QueueLow      = "low"
+)
+
+type TaskProcessor interface {
+	Start() error
+	ProcessTaskSendVerifyEmail(ctx context.Context, task *asynq.Task) error
+}
+
+type RedisTaskProcessor struct {
+	server *asynq.Server
+	store  db.Store
+}
+
+func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	server := asynq.NewServer(
+		redisOpt,
+		asynq.Config{
+			Queues: map[string]int{
+				QueueCritical: 10,
+				QueueDefault:  5,
+				QueueLow:      1,
+			},
+		},
+	)
+
+	return &RedisTaskProcessor{
+		server: server,
+		store:  store,
+	}
+}
+
+func (processor *RedisTaskProcessor) Start() error {
+	mux := asynq.NewServeMux()
+
+	mux.HandleFunc(TaskSendVerifyEmail, processor.ProcessTaskSendVerifyEmail)
+
+	return processor.server.Start(mux)
+}
